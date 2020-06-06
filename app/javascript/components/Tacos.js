d7dec261e1a905865f4deef8cdd2cc7d9df92285
Jason Terhorst <yourstruly@jterhorst.com>
2020-06-05 23:41:10 -0500

    JSDoc (#7)

--- app/javascript/components/Tacos.js
+++ app/javascript/components/Tacos.js
@@ -1,10 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+/**
+ * Component for showing list of tacos.
+ * 
+ * @component
+ * @example
+ * const title = "Awesome Taco List"
+ * const tacos = []
+ * return (
+ *   <Tacos title={title} tacos={tacos} />
+ * )
+ */
 class Tacos extends React.Component {
   render () {
     return (
       <div>
-        <h1>Tacos</h1>
+        <h1>{this.props.title}</h1>
         <ul>
           {this.props.tacos.map(taco => (
             <li key={taco.id}>{`${taco.name} ${taco.description}`}</li>
@@ -16,6 +28,13 @@ class Tacos extends React.Component {
 }
 
 Tacos.propTypes = {
-  tacos: PropTypes.array
+    title: PropTypes.string.isRequired,
+    tacos: PropTypes.array.isRequired,
 };
+
+Tacos.defaultProps = {
+    title: 'Tacos',
+    tacos: [],
+}
+
 export default Tacos
